e1f968866b0dd5a46f921e500c3e2e6dcc89f495
idegen <degen.isabella@gmail.com>
2025-01-22 10:59:28 +0000

    defined the 'prod' configuration to calcualte distance measures raw criteria

--- src/evaluation/run_distance_evaluation_raw_criteria.py
+++ src/evaluation/run_distance_evaluation_raw_criteria.py
@@ -1,4 +1,9 @@
+import pandas as pd
+
 from src.evaluation.distance_metric_evaluation import DistanceMetricEvaluation
+from src.utils.configurations import ROOT_RESULTS_DIR, GENERATED_DATASETS_FILE_PATH, SYNTHETIC_DATA_DIR, \
+    IRREGULAR_P30_DATA_DIR, IRREGULAR_P90_DATA_DIR
+from src.utils.distance_measures import DistanceMeasures
 from src.utils.load_synthetic_data import SyntheticDataType
 from src.utils.plots.matplotlib_helper_functions import Backends
 
@@ -15,5 +20,34 @@ def run_distance_evaluation_raw_criteria_for_ds(data_dirs: [str], dataset_types:
 
 
 if __name__ == "__main__":
-    overall_ds_name = "n2"
-    # run_distance_evaluation_raw_criteria_for_ds(overall_ds_name)
+    # configuration to calculate raw distance measure criteria for the N30 dataset
+    root_result_dir = ROOT_RESULTS_DIR
+    dataset_types = [SyntheticDataType.raw,
+                     SyntheticDataType.normal_correlated,
+                     SyntheticDataType.non_normal_correlated,
+                     SyntheticDataType.rs_1min]
+    data_dirs = [SYNTHETIC_DATA_DIR,
+                 IRREGULAR_P30_DATA_DIR,
+                 IRREGULAR_P90_DATA_DIR]
+
+    # this is an extensive list
+    distance_measures = [DistanceMeasures.l1_cor_dist,  # lp norms
+                         DistanceMeasures.l2_cor_dist,
+                         DistanceMeasures.l3_cor_dist,
+                         DistanceMeasures.l5_cor_dist,
+                         DistanceMeasures.linf_cor_dist,
+                         DistanceMeasures.l1_with_ref,  # lp norms with reference vector
+                         DistanceMeasures.l2_with_ref,
+                         DistanceMeasures.l3_with_ref,
+                         DistanceMeasures.l5_with_ref,
+                         DistanceMeasures.linf_with_ref,
+                         DistanceMeasures.dot_transform_l1,  # dot transform + lp norms
+                         DistanceMeasures.dot_transform_l2,
+                         DistanceMeasures.dot_transform_linf,
+                         DistanceMeasures.log_frob_cor_dist,  # correlation metrix
+                         DistanceMeasures.foerstner_cor_dist]
+
+    run_names = pd.read_csv(GENERATED_DATASETS_FILE_PATH)['Name'].tolist()
+
+    run_distance_evaluation_raw_criteria_for_ds(data_dirs=data_dirs, dataset_types=dataset_types, run_names=run_names,
+                                                root_result_dir=root_result_dir, distance_measures=distance_measures)
