382d41bbfbe849abca4c75a0517bf626a2917e86
idegen <degen.isabella@gmail.com>
2025-02-25 18:52:16 +0000

    create script to create confirmatory data dir

---
+++ src/data_generation/reduced_datasets/confirmatory/run_create_reduced_datasets.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+from src.data_generation.reduced_datasets.run_create_reduced_datasets import create_reduced_datasets
+from src.utils.configurations import ROOT_REDUCED_SYNTHETIC_DATA_DIR, DataCompleteness, CONFIRMATORY_DATASETS_FILE_PATH, \
+    CONFIRMATORY_SYNTHETIC_DATA_DIR
+from src.utils.load_synthetic_data import SyntheticDataType
+
+if __name__ == "__main__":
+    _run_names = pd.read_csv(CONFIRMATORY_DATASETS_FILE_PATH)['Name'].tolist()
+    _seed = 600
+
+    _data_types = [SyntheticDataType.normal_correlated, SyntheticDataType.non_normal_correlated]
+    _completeness = [DataCompleteness.complete, DataCompleteness.irregular_p30, DataCompleteness.irregular_p90]
+
+    _root_reduced_data_dir = ROOT_REDUCED_SYNTHETIC_DATA_DIR
+
+    create_reduced_datasets(root_data_dir=CONFIRMATORY_SYNTHETIC_DATA_DIR, data_types=_data_types,
+                            completeness=_completeness, run_names=_run_names, seed=_seed,
+                            root_reduced_data_dir=_root_reduced_data_dir)
--- src/data_generation/reduced_datasets/run_create_reduced_datasets.py
+++ src/data_generation/reduced_datasets/run_create_reduced_datasets.py
@@ -9,23 +9,15 @@ from src.utils.configurations import GENERATED_DATASETS_FILE_PATH, SYNTHETIC_DAT
     get_root_folder_for_reduced_segments, get_data_dir, DataCompleteness
 from src.utils.load_synthetic_data import SyntheticDataType
 
-if __name__ == "__main__":
-    overall_dataset_name = "n30"
-    run_names = pd.read_csv(GENERATED_DATASETS_FILE_PATH)['Name'].tolist()
-    seed = 600
 
-    data_types = [SyntheticDataType.normal_correlated, SyntheticDataType.non_normal_correlated]
-    completeness = [DataCompleteness.complete, DataCompleteness.irregular_p30, DataCompleteness.irregular_p90]
-
-    root_reduced_data_dir = ROOT_REDUCED_SYNTHETIC_DATA_DIR
-
+def create_reduced_datasets(root_data_dir: str, data_types: [str], completeness: [str], run_names: [str], seed: int,
+                            root_reduced_data_dir: str):
     # drop 50% and 75% of clusters and segments
     n_dropped_clusters = [12, 17]
     n_dropped_segments = [50, 75]
-
     for comp in completeness:
         for data_type in data_types:
-            data_dir = get_data_dir(SYNTHETIC_DATA_DIR, comp)  # to read data
+            data_dir = get_data_dir(root_data_dir, comp)  # to read data
             rd = CreateReducedDatasets(run_names=run_names, data_type=SyntheticDataType.normal_correlated,
                                        data_dir=data_dir, drop_n_clusters=n_dropped_clusters,
                                        drop_n_segments=n_dropped_segments, base_seed=seed)
@@ -72,3 +64,17 @@ if __name__ == "__main__":
                                              reduced_segments_data[n_dropped_seg][run_name],
                                              reduced_segments_labels[n_dropped_seg][run_name],
                                              run_name)
+
+
+if __name__ == "__main__":
+    _run_names = pd.read_csv(GENERATED_DATASETS_FILE_PATH)['Name'].tolist()
+    _seed = 600
+
+    _data_types = [SyntheticDataType.normal_correlated, SyntheticDataType.non_normal_correlated]
+    _completeness = [DataCompleteness.complete, DataCompleteness.irregular_p30, DataCompleteness.irregular_p90]
+
+    _root_reduced_data_dir = ROOT_REDUCED_SYNTHETIC_DATA_DIR
+
+    create_reduced_datasets(root_data_dir=SYNTHETIC_DATA_DIR, data_types=_data_types, completeness=_completeness,
+                            run_names=_run_names, seed=_seed,
+                            root_reduced_data_dir=_root_reduced_data_dir)
