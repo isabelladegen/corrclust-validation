0a98680b294422595390919d66fed062215f01d3
idegen <degen.isabella@gmail.com>
2025-03-06 14:01:08 +0000

    removed some duplicated code that was unused

--- src/experiments/run_impact_of_distance_measure_assessment.py
+++ src/experiments/run_impact_of_distance_measure_assessment.py
@@ -11,46 +11,6 @@ from src.utils.distance_measures import DistanceMeasures
 from src.utils.load_synthetic_data import SyntheticDataType
 
 
-def assess_internal_measures(overall_dataset_name: str, run_names: [str], data_type: str,
-                             root_results_dir: str, data_dir: str,
-                             distance_measure: str,
-                             internal_measures: [str], n_clusters=0, n_segments=0):
-    # load all the internal measure calculation summaries
-    partitions = read_clustering_quality_measures(overall_ds_name=overall_dataset_name, data_type=data_type,
-                                                  root_results_dir=root_results_dir, data_dir=data_dir,
-                                                  distance_measure=distance_measure, run_names=run_names)
-
-    ia = InternalMeasureAssessment(distance_measure=distance_measure, dataset_results=partitions,
-                                   internal_measures=internal_measures)
-
-    store_results_in = internal_measure_evaluation_dir_for(
-        overall_dataset_name=overall_dataset_name,
-        data_type=data_type,
-        results_dir=root_results_dir, data_dir=data_dir,
-        distance_measure=distance_measure,
-        drop_segments=n_segments, drop_clusters=n_clusters)
-
-    # correlation summary
-    ia.correlation_summary.to_csv(
-        get_full_filename_for_results_csv(store_results_in, IAResultsCSV.correlation_summary))
-
-    # effect size between difference of mean correlation of worst and gt
-    ia.differences_between_worst_and_best_partition().to_csv(
-        get_full_filename_for_results_csv(store_results_in, IAResultsCSV.effect_size_difference_worst_best))
-
-    # descriptive statistics
-    ia.descriptive_statistics_for_internal_measures_correlation().to_csv(
-        get_full_filename_for_results_csv(store_results_in, IAResultsCSV.descriptive_statistics_measure_summary))
-
-    # 95% CI of differences in mean correlation between internal measures
-    ia.ci_of_differences_between_internal_measure_correlations().to_csv(
-        get_full_filename_for_results_csv(store_results_in, IAResultsCSV.ci_of_differences_between_measures))
-
-    # paired samples t test on fisher transformed correlation coefficients
-    df = ia.paired_samples_t_test_on_fisher_transformed_correlation_coefficients(alpha=0.05, alternative='two-sided')
-    df.to_csv(get_full_filename_for_results_csv(store_results_in, IAResultsCSV.paired_t_test))
-
-
 def run_impact_of_distance_measure_on_data_variant(overall_ds_name: str, run_names: [str], distance_measures: [str],
                                                    data_type: str, data_dir: str, results_dir: str,
                                                    internal_measures: [str], n_dropped_clusters: [int] = [],
@@ -73,8 +33,7 @@ def run_impact_of_distance_measure_on_data_variant(overall_ds_name: str, run_nam
 if __name__ == "__main__":
     overall_ds_name = "n30"
     root_result_dir = ROOT_RESULTS_DIR
-    dataset_types = [SyntheticDataType.raw,
-                     SyntheticDataType.normal_correlated,
+    dataset_types = [SyntheticDataType.normal_correlated,
                      SyntheticDataType.non_normal_correlated,
                      SyntheticDataType.rs_1min]
     data_dirs = [SYNTHETIC_DATA_DIR,
